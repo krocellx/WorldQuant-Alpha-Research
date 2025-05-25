eeff640c60e851535d1d8c272d9086628015d8d2
Harvey Xie <Harvey_xie@hotmail.com>
2025-05-25 14:57:21 +0100

    Update tracker file name and path in main simulation script

--- main_simulation.py
+++ main_simulation.py
@@ -1,9 +1,9 @@
-
+import os
 from src.core.wq_simulation import WQSimulation
 
 
 if __name__ == "__main__":
-    TRACKER_FILE = 'alpha_tracking.csv'
-
+    TRACKER_FILE = 'alpha_tracking_sample.csv'
     SIMULATOR = WQSimulation()
-    SIMULATOR.run_simulation_from_tracker(TRACKER_FILE)
+    SIMULATOR.run_simulation_from_tracker(os.path.join('alpha', TRACKER_FILE))
+
